dac8d3e707be90740515e906c829f5910beee0c4
b-rabbit611 <b.rabbit666@gmail.com>
2016-05-13 23:07:59 +0900

    modify

--- .emacs.d/site-lisp/linux.el
+++ .emacs.d/site-lisp/linux.el
@@ -1,1 +1,12 @@
 ;; for Linux
+;; ターミナルから呼び出したときにターミナルに
+;; 渡す文字コード
+(set-terminal-coding-system 'utf-8-unix)
+
+;; 新しく開いたファイルを保存しておくときの
+;; 文字コード
+(prefer-coding-system 'utf-8-unix)
+
+;; emacsをXのアプリケーションへ貼り付ける
+;; ときの文字コード
+(set-clipboard-coding-system 'utf-8)
